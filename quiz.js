2f3632e60e6987acdc019add5486c65f677e1aba
Stirling-Silver <113132128+Stirling-Silver@users.noreply.github.com>
2022-12-02 11:22:41 -0700

    AAAAAAAAAAAA, the button doesn't work

--- quiz.js
+++ quiz.js
@@ -1,15 +1,25 @@
-document.getElementById("btn").addEventListener("click", grading);
+document.getElementById("grading").addEventListener("click", Grading);
 
-function grading() {
+document.getElementById(`Q1`);
+document.getElementById(`Q2`);
+document.getElementById(`Q3`);
+document.getElementById(`Q4`);
+document.getElementById(`Q5`);
+document.getElementById(`Q6`);
+
+function Grading() {
   alert("Click");
-  let Q1G = Q1;
-  let Q2G = Q2;
-  let Q3G = Q3;
-  let Q4G = Q4;
-  let Q5G = Q5;
-  let Q6G = Q6;
+  let Q1G = document.getElementById(`Q1`);
+  let Q2G = document.getElementById(`Q2`);
+  let Q3G = document.getElementById(`Q3`);
+  let Q4G = document.getElementById(`Q4`);
+  let Q5G = document.getElementById(`Q5`);
+  let Q6G = document.getElementById(`Q6`);
   let GPAG = (Q1G + Q2G + Q3G + Q4G + Q5G + Q6G) / 5;
-}
 
+  if (Q1G === "Parital Lobes" || Q1G === "The Parital Lobes") {
+    alert("Ding");
+  }
+}
 let GPAG = GPA;
 document.getElementById("GPA").innerHTML = `Grade: ${GPA}`;
