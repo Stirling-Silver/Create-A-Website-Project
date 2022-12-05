d9a6100bd335c4bbd59803a608a62be2c0af4e53
Stirling-Silver <113132128+Stirling-Silver@users.noreply.github.com>
2022-12-05 11:21:11 -0700

    Fixed the dumb mistake, work on the grading system

--- quiz.js
+++ quiz.js
@@ -1,4 +1,4 @@
-document.getElementById("grading").addEventListener("click", Grading);
+document.getElementById("Gbtn").addEventListener("click", Grading);
 
 document.getElementById(`Q1`);
 document.getElementById(`Q2`);
@@ -8,18 +8,48 @@ document.getElementById(`Q5`);
 document.getElementById(`Q6`);
 
 function Grading() {
-  alert("Click");
-  let Q1G = document.getElementById(`Q1`);
-  let Q2G = document.getElementById(`Q2`);
-  let Q3G = document.getElementById(`Q3`);
-  let Q4G = document.getElementById(`Q4`);
-  let Q5G = document.getElementById(`Q5`);
-  let Q6G = document.getElementById(`Q6`);
-  let GPAG = (Q1G + Q2G + Q3G + Q4G + Q5G + Q6G) / 5;
-
+  let Q1G = document.getElementById(`Q1`).value;
+  let Q2G = document.getElementById(`Q2`).value;
+  let Q3G = document.getElementById(`Q3`).value;
+  let Q4G = document.getElementById(`Q4`).value;
+  let Q5G = document.getElementById(`Q5`).value;
+  let Q6G = document.getElementById(`Q6`).value;
   if (Q1G === "Parital Lobes" || Q1G === "The Parital Lobes") {
-    alert("Ding");
+    console.log;
+    GPAm++;
+  } else {
+    GPAm + 0;
+  }
+  if (Q2G === "" || Q2G === "") {
+    console.log;
+    GPAG++;
+  } else {
+    GPAG = 0;
+  }
+  if (Q3G === "" || Q3G === "") {
+    console.log;
+    GPAG++;
+  } else {
+    GPAG + 0;
+  }
+  if (Q4G === "Parital Lobes" || Q4G === "The Parital Lobes") {
+    console.log;
+    GPAG++;
+  } else {
+    GPAG + 0;
+  }
+  if (Q5G === "Parital Lobes" || Q5G === "The Parital Lobes") {
+    console.log;
+    GPAG++;
+  } else {
+    GPAG + 0;
+  }
+  if (Q6G === "Parital Lobes" || Q6G === "The Parital Lobes") {
+    alert(GPAG);
+    GPAG++;
+  } else {
+    GPAG + 0;
   }
 }
-let GPAG = GPA;
-document.getElementById("GPA").innerHTML = `Grade: ${GPA}`;
+alert(GPAG);
+document.getElementById("GPA").innerHTML = `Grade: ${GPAG}`;
