339f74fed8bbe42c6c95331c5c7ee58f59afa9c7
Stirling-Silver <113132128+Stirling-Silver@users.noreply.github.com>
2022-12-08 11:23:30 -0700

    FIXED

--- quiz.js
+++ quiz.js
@@ -1,6 +1,5 @@
 document.getElementById("Grading").addEventListener("click", Grading);
 let T = 0;
-let GPA = 0;
 
 function Grading() {
   let Q1G = document.getElementById("Q1").value;
@@ -12,41 +11,50 @@ function Grading() {
 
   if (Q1G === "Parital Lobes" || Q1G === "The Parital Lobes") {
     document.getElementById("Q1R").innerHTML = "Correct";
-    T++;
+    console.log((T += 1));
   } else {
     document.getElementById("Q1R").innerHTML = "Incorrect";
   }
   if (Q2G === "Nervous System" || Q2G === "The Nervous System") {
     document.getElementById("Q2R").innerHTML = "Correct";
-    T++;
+    console.log((T += 1));
   } else {
     document.getElementById("Q2R").innerHTML = "Incorrect";
   }
   if (Q3G === "Optical Nerve" || Q3G === "The Optical Nerve") {
     document.getElementById("Q3R").innerHTML = "Correct";
-    T++;
+    console.log((T += 1));
   } else {
     document.getElementById("Q3R").innerHTML = "Incorrect";
   }
   if (Q4G === "Hippocampus" || Q4G === "The Hippocampus") {
     document.getElementById("Q4R").innerHTML = "Correct";
-    T++;
+    console.log((T += 1));
   } else {
     document.getElementById("Q4R").innerHTML = "Incorrect";
   }
   if (Q5G === "Pre-Frontal Lobe" || Q5G === "The Pre-Frontal Lobe") {
     document.getElementById("Q5R").innerHTML = "Correct";
-    T++;
+    console.log((T += 1));
   } else {
     document.getElementById("Q5R").innerHTML = "Incorrect";
   }
   if (Q6G === "Heart" || Q6G === "The Heart") {
     document.getElementById("Q6R").innerHTML = "Correct";
-    T++;
+    console.log((T += 1));
   } else {
     document.getElementById("Q6R").innerHTML = "Incorrect";
   }
+  let FT = (T / 6) * 100;
+  document.getElementById("GPA").innerHTML = `Final Grade: ${T}/6 (${FT}%)`;
+  document.getElementById("GPA").addEventListener("click", Grading);
+
+  if (T === 0) {
+    document.getElementById("R").innerHTML = "This is a massive skill issue.";
+  } else if () {
+    document.getElementById("R").innerHTML = "This is a massive skill issue.";
+  } else if () {
+
+  }
 }
-let FT = (T / 6) * 100;
-document.getElementById("GPA").innerHTML = `Final Grade: ${FT}%`;
-document.getElementById("GPA").addEventListener("click", Grading);
+//Need 6 if statments
